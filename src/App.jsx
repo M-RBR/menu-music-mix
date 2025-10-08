ea0a5984cf42a548f542ffd569a396908f27179d
maximrubenbenjamin <roozenma@gmail.com>
2025-10-08 11:50:23 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>MENU MUSIC MIX 🎶</h1>
+      <p className="font-style: italic">made in Berlin, 1900-1914</p>
+    </>
+  );
+}
+
+export default App;
